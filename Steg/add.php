a36cdde11fabb92337734dd7c89d30749ed6c05e
Martin Pedersen <martin.pedersen@me.com>
2018-02-28 12:40:08 +0100

    Created and added repository

---
+++ Steg/add.php
@@ -0,0 +1,12 @@
+<?php $title = "eLearning"; ?>
+
+<!--?php include('html/menu.html'); ?-->
+<!-- include andre sider etter hvordan du vill ha siden til å se ut-->
+<!-- Hvilken html fill som du vill, dette vill bestemme innholdet ditt -->
+<?php include ('../html/header.php'); ?>
+
+<?php include ('../html/git-add.html'); ?>
+
+<?php include ('../html/scripts.html'); ?>
+
+<?php include ('../html/footer.html'); ?>
---
+++ Steg/youtube.php
@@ -0,0 +1,19 @@
+<?php $title = "eLearning"; ?>
+<?php include ('../html/header.php'); ?>
+
+
+<div class="videowrapper">
+
+  <h1 class="text-center">Introduksjonsvideo om Git og GitHub</h1><hr>
+  <iframe width="600" height="300" src="https://www.youtube.com/embed/DR7MLaAKcUk?rel=0" frameborder="0" allowfullscreen></iframe>
+
+
+  <div class="row knapper" id="dyttKnapp">
+      <a href="Steg/init.php" type="button" class="btn btn-success btn-block">Fortsett</a>
+  </div>
+</div>
+
+
+
+<?php include ('../html/scripts.html'); ?>
+<?php include ('../html/footer.html'); ?>
